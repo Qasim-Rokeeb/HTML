0db42017f4c43559c307402d02a0c144daf9d7b9
Qasim Rokeeb <117784021+Qasim-Rokeeb@users.noreply.github.com>
2026-01-30 21:44:22 +0100

    began js classes

---
+++ JavaScript/variables.js
@@ -0,0 +1,18 @@
+let x = "Jessica";
+
+let y = "Joseph";
+
+let isValid = false;
+
+y = "Emily";
+
+z = 123;
+let p;
+
+console.log(typeof isValid);
+console.log(typeof z);
+console.log(typeof y);
+console.log(typeof p);
+
+
+console.log(y + " and " + x + " are friends.");
